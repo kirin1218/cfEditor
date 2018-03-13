12bb4933f5eea17a9ade2f4fd2813e719ce6aad7
kirin1218 <kirin1218@outlook.jp>
2018-03-13 23:18:44 +0900

    init

---
+++ cfEditor.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import sys
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250,250)
+    w.setWindowTitle("Command File Editor")
+
+    table = QTableWidget(w)
+    #tableItem = QTableWidgetItem()
+
+    table.setRowCount(2)
+    table.setColumnCount(2)
+
+    horzHeaders = ["Name", "Age"]
+    table.setHorizontalHeaderLabels( horzHeaders )
+
+    table.setItem(0,0, QTableWidgetItem("tom"))
+    table.setItem(0,1, QTableWidgetItem("15"))
+    table.setItem(1,0, QTableWidgetItem("Ken"))
+    table.setItem(1,1, QTableWidgetItem("44"))
+    
+    w.show()
+
+    sys.exit(app.exec_())
