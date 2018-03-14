898b92c8f5e5f13ff133e0a87f36f0eb520f75eb
kirin1218 <kirin1218@outlook.jp>
2018-03-14 18:57:06 +0900

    pep8 check

--- cf.py
+++ cf.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-
 class command_file():
-    def __init__( self, cfpath ):
+    def __init__(self, cfpath):
         self.path = cfpath
 
         # open and read cffile
@@ -11,14 +10,10 @@ class command_file():
         else:
             print("open error")
 
-
-    def read( self ):
+    def read(self):
         try:
             f = open(self.path, 'r')
         except IOError:
             print('"%s" cannot be opned.', self.path)
             return 0
-        
         return f
-
-
